978a9dd8caf0aa127450da00cae89a9bd016973f
Jinen Gandhi <jinen.gandhi@intel.com>
2020-03-05 16:42:58 +0530

    POC commit for testing proxy model

--- tests/automation_framework/globals.py
+++ tests/automation_framework/globals.py
@@ -1,3 +1,15 @@
 from avalon_sdk.http_client.http_jrpc_client \
         import HttpJrpcClient
 uri_client = HttpJrpcClient("http://avalon-listener:1947")
+
+proxy_worker_registry_contract_address = "0x75a3Fd17E8c5CceAa9121251c359bFe4b9C343C8"
+
+work_order_contract_address = "0xf873133fae1d1f80642c551a6edd5A14f37129c2"
+
+eth_account = "0x7085d4d4c6efea785edfba5880bb62574e115626"
+
+provider = "http://10.66.241.55:22001"
+
+event_provider = "http://10.66.241.55:22011"
+
+blockchain = "ethereum"

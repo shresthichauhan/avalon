11deb170bb9d1bbd90c6da78f1cd028a658670c4
shresthichauhan <35533098+shresthichauhan@users.noreply.github.com>
2020-08-19 20:18:13 +0530

    Update conf_matrix.sh

--- conf_matrix.sh
+++ conf_matrix.sh
@@ -1,17 +1,21 @@
+173 – ubuntu18 :
+mount.cifs //ip-address/Share/Jenkins /Jenkins  -o user=administrator pwd=intel@123
+
+
 #!/bin/sh
 Date=`date +%Y-%m-%d`
 export RUNID="Nightly"_${Date}
-DIR="/Jenkins/SDPTool/$RUNID"
+DIR="/Jenkins/SDPTool/$RUNID/SDPTool-1.3-11/RPMS/"
 mkdir -p $DIR
 cd $DIR
-SUB_DIR="SDPTool-1.3-11/RPMS/ubuntu_18_x/"
-mkdir -p $SUB_DIR
+DISTRO="ubuntu_18_x/"
+mkdir -p $DISTRO
 
 cd /Jenkins/dcg_csw_platform_software-intelcli/build
 echo "================= $NODE_NAME"
 if [ "ubuntu18_x_jenkins" = "$NODE_NAME" ]; then
     echo "================= This is copy final result ===================="
-    cp RPMS/SDPTool-1.3-11*.* "$DIR/$SUB_DIR"
+    cp RPMS/SDPTool-1.3-11*.* "$DIR/$DISTRO"
 else
     echo "================Not Copying the final result =================="
 fi
